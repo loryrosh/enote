491f8dfa156a77ac11bdaf0550f5471a7480914f
Iren <nor.iren@yandex.ru>
2017-12-04 23:16:45 +0300

    fix some things

--- data/db.sql
+++ data/db.sql
@@ -6,10 +6,11 @@
 DROP TABLE IF EXISTS `notebook`;
 		
 CREATE TABLE `notebook` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `user_id` INTEGER NOT NULL DEFAULT NULL,
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
+  `date` DATE NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Table 'label'
@@ -19,14 +20,8 @@ CREATE TABLE `notebook` (
 DROP TABLE IF EXISTS `label`;
 		
 CREATE TABLE `label` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR NULL DEFAULT NULL,
-  `name` VARCHAR NULL DEFAULT NULL,
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-);
-
--- ---
--- Foreign Keys 
--- ---
-
-ALTER TABLE `notebook` ADD FOREIGN KEY (user_id) REFERENCES `user` (`id`);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
