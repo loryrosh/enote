029b8d35edede926e3431478c90d99be0129eea4
Iren <nor.iren@yandex.ru>
2017-12-05 00:02:41 +0300

    fix to lowercase

--- data/db.sql
+++ data/db.sql
@@ -6,9 +6,9 @@
 DROP TABLE IF EXISTS `notebook`;
 		
 CREATE TABLE `notebook` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `user_id` INTEGER NOT NULL,
-  `date` DATE NOT NULL DEFAULT NOW(),
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `date` date NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -20,8 +20,8 @@ CREATE TABLE `notebook` (
 DROP TABLE IF EXISTS `label`;
 		
 CREATE TABLE `label` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `color` INTEGER NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `color` int NOT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
