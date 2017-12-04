bb2e2413b1fec23b2ecacfba5fceeb87c2e525a6
Iren <nor.iren@yandex.ru>
2017-12-04 23:42:17 +0300

    fix

--- data/db.sql
+++ data/db.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `notebook`;
 		
 CREATE TABLE `notebook` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `user_id` INTEGER NULL DEFAULT NULL,
+  `user_id` INTEGER NOT NULL,
   `date` DATE NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -21,7 +21,13 @@ DROP TABLE IF EXISTS `label`;
 		
 CREATE TABLE `label` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) NOT NULL,
+  `color` varchar(6) NOT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `notebook` ADD FOREIGN KEY (user_id) REFERENCES `user` (`id`);
