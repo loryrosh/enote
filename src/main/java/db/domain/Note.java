b0746853eb3d2a12e32c4c182e6347ba673b28f4
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-06 00:30:36 +0300

    [ENote] Add Note entity

--- src/main/java/db/domain/Note.java
+++ src/main/java/db/domain/Note.java
@@ -1,7 +1,75 @@
 package db.domain;
 
 import db.domain.util.BaseEntity;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Note extends BaseEntity {
 
+    @ManyToOne
+    @JoinColumn(name = "user_id" )
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "notebook_id" )
+    private Notebook notebook;
+
+    @Basic
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date date;
+
+    @ManyToMany
+    @JoinTable(name = "note_attachment",
+            joinColumns = @JoinColumn(name = "note_id"),
+            inverseJoinColumns = @JoinColumn(name = "attachment_id")
+    )
+    private List<Attachment> attachments = new ArrayList<>();
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Notebook getNotebook() {
+        return notebook;
+    }
+
+    public void setNotebook(Notebook notebook) {
+        this.notebook = notebook;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Attachment> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(List<Attachment> attachments) {
+        this.attachments = attachments;
+    }
 }
