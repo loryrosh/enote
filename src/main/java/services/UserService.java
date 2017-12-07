919d864f930b42a8fc47b91a7230b379ebd0e0b7
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-07 08:42:24 +0300

    [ENote] Add md5digest function for user password

---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,10 @@
+package services;
+
+import org.springframework.util.DigestUtils;
+
+public class UserService {
+
+    public String getEncodedPassword(String password) {
+        return DigestUtils.	md5DigestAsHex(password.getBytes());
+    }
+}
