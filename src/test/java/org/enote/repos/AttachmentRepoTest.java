146dd30c4642b71c7359638d18b4e2a36c9c078f
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-09 22:24:49 +0300

    [ENote]  Add assertion to Attachment test

--- src/test/java/org/enote/repos/AttachmentRepoTest.java
+++ src/test/java/org/enote/repos/AttachmentRepoTest.java
@@ -2,21 +2,19 @@ package org.enote.repos;
 
 import org.enote.config.DataSourceConfigTest;
 import org.enote.config.TestPreConfig;
-import org.enote.config.db.DataSourceConfig;
-import org.enote.config.db.PersistenceConfig;
 import org.enote.domain.Attachment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {PersistenceConfig.class, DataSourceConfig.class})
 public class AttachmentRepoTest extends TestPreConfig {
 
     private Logger logger = LoggerFactory.getLogger(DataSourceConfigTest.class);
@@ -28,6 +26,8 @@ public class AttachmentRepoTest extends TestPreConfig {
     public void checkTotalAmount() {
         long count = attachmentRepo.count();
         logger.info("Total amount of attachment types: " + count);
+
+        assertEquals(4, count);
     }
 
     @Test
