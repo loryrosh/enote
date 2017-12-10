b156a838935265f84ee2b5c72a4c054a52651f3e
Alexandra Roshchina <Alexandra_Roshchina@epam.com>
2017-12-10 13:39:40 +0300

    [ENote] Add notebook create/update test

--- src/main/java/org/enote/services/impl/NotebookServiceImpl.java
+++ src/main/java/org/enote/services/impl/NotebookServiceImpl.java
@@ -1,7 +1,21 @@
 package org.enote.services.impl;
 
+import org.enote.domain.Note;
+import org.enote.domain.Notebook;
+import org.enote.repos.NotebookRepo;
 import org.enote.services.NotebookService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class NotebookServiceImpl implements NotebookService {
 
+    @Autowired
+    private NotebookRepo notebookRepo;
+
+    @Override
+    public Note saveNote(Notebook notebook, Note note) {
+        notebook.setNote(note);
+        notebookRepo.save(notebook);
+
+        return note;
+    }
 }
